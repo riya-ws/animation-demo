56b2e132c5efd5040b6ebe98f259cc7c651c8925
riya-ws <riya@whitestorksoft.com>
2024-10-16 15:47:10 +0530

    Demo two on scroll sticky card stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import "./App.css";
-import ThirdTestimonials from "./assets/components/third-testimonials";
+import Work from "./assets/components/work";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <SimpleSlider /> */}
-      {/* <HomeSlider /> */}
-      <h1 className="text-xl font-medium text-center my-3">Third demo</h1>
-      <ThirdTestimonials />
       {/*On scroll how to works section*/}
-      {/* <StickyLayers /> */}
+      <Work />
     </>
   );
 }
