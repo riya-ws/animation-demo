62823fa398734cc236ca1e35036a4adf34e6c3af
riya-ws <riya@whitestorksoft.com>
2024-10-17 15:49:08 +0530

    on scroll colour change and real time changes remove gradient and add image

--- src/assets/components/scroll-reveal.tsx
+++ src/assets/components/scroll-reveal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { stickyRevealData } from "../../utils/constant";
+import stickyReveal from "../../assets/linear.png";
 
 const ScrollReveal = () => {
   const [currentItem, setCurrentItem] = useState(stickyRevealData[0]);
@@ -32,8 +33,14 @@ const ScrollReveal = () => {
 
   return (
     <>
-      <div  className=" bg-black flex w-full gap-10 my-10 overflow-auto">
-        <div className="h-[30rem] flex justify-center flex-col relative space-x-10 rounded-md container mx-auto">
+      <div
+        className={`bg-black flex w-full gap-10 my-10 overflow-y-hidden ${
+          currentItem.title === "Real time changes"
+            ? "bg-black"
+            : "bg-[#0f172a]"
+        }`}
+      >
+        <div className="overflow-y-auto flex justify-center flex-col relative space-x-10 p-10 rounded-md container mx-auto">
           <div className="flex w-full">
             {/* Left Content */}
             <div className="flex flex-col gap-10 items-start w-[60%]">
@@ -50,8 +57,8 @@ const ScrollReveal = () => {
                         : "text-gray-400 opacity-50"
                     }`}
                   >
-                    {/* <h1 className="text-2xl font-medium">{item.title}</h1> */}
-                    <p className="text-4xl font-bold">{item.content}</p>
+                    <h1 className="text-4xl font-bold">{item.title}</h1>
+                    {/* <p className="text-4xl font-bold">{item.content}</p> */}
                     <p className="text-lg font-normal">{item.description}</p>
                   </div>
                 </div>
@@ -60,11 +67,28 @@ const ScrollReveal = () => {
             {/* Sticky Box */}
             <div className="w-[40%] flex items-center justify-center">
               <div
-                className="sticky top-20 w-80 h-80 rounded-2xl flex flex-col items-center justify-center transition-all duration-500 z-10"
-                style={{ background: bgGradient }}
+                className={`sticky top-20 w-80 h-80 rounded-2xl flex flex-col items-center justify-center transition-all duration-500 z-10 ${
+                  currentItem.title === "Real time changes" ? "" : "bg-gradient"
+                }`}
+                style={{
+                  background:
+                    currentItem.title === "Real time changes"
+                      ? "none"
+                      : bgGradient,
+                }}
               >
+                {currentItem.title === "Real time changes" && (
+                  <img
+                    src={stickyReveal}
+                    width={300}
+                    height={300}
+                    className="h-full w-full object-cover rounded-2xl"
+                    alt="linear board demo"
+                  />
+                )}
                 <h2 className="text-xl font-bold text-center text-white">
-                  {currentItem.title}
+                  {currentItem.title !== "Real time changes" &&
+                    currentItem.title}
                 </h2>
               </div>
             </div>
