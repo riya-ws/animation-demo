11237b45c984546044940403becba613dcc88500
riya-ws <riya@whitestorksoft.com>
2024-10-18 18:11:05 +0530

    Slider done with scrolling

--- src/assets/components/infinite-card-scroll.tsx
+++ src/assets/components/infinite-card-scroll.tsx
@@ -31,84 +31,95 @@ const InfiniteCardScroll = () => {
   };
 
   return (
-    <div className="infinite-scroll">
-      <div className="gallery">
-        <ul className="cards" onWheel={handleScroll}>
-          {cardsArray.map((card, index) => {
-            const isActive = index === activeIndex;
-            const isNext1 = index === (activeIndex + 1) % cardsArray.length;
-            const isNext2 = index === (activeIndex + 2) % cardsArray.length;
-            const isNext3 = index === (activeIndex + 3) % cardsArray.length;
-            const isPrev1 =
-              index ===
-              (activeIndex - 1 + cardsArray.length) % cardsArray.length;
-            const isPrev2 =
-              index ===
-              (activeIndex - 2 + cardsArray.length) % cardsArray.length;
-            const isPrev3 =
-              index ===
-              (activeIndex - 3 + cardsArray.length) % cardsArray.length;
+    <>
+      <div className="infinite-scroll">
+        <div className="gallery">
+          <ul className="cards" onWheel={handleScroll}>
+            {cardsArray.map((card, index) => {
+              const isActive = index === activeIndex;
+              const isNext1 = index === (activeIndex + 1) % cardsArray.length;
+              const isNext2 = index === (activeIndex + 2) % cardsArray.length;
+              const isNext3 = index === (activeIndex + 3) % cardsArray.length;
+              const isPrev1 =
+                index ===
+                (activeIndex - 1 + cardsArray.length) % cardsArray.length;
+              const isPrev2 =
+                index ===
+                (activeIndex - 2 + cardsArray.length) % cardsArray.length;
+              const isPrev3 =
+                index ===
+                (activeIndex - 3 + cardsArray.length) % cardsArray.length;
 
-            console.log(isNext2, isPrev2, isNext3, isPrev3);
-            return (
-              <li
-                key={index}
-                className={`card ${isActive ? "active" : ""} ${
-                  isNext1 ? "next1" : ""
-                } ${isNext2 ? "next2" : ""} ${isNext3 ? "next3" : ""} ${
-                  isPrev1 ? "prev1" : ""
-                } ${isPrev2 ? "prev2" : ""} ${isPrev3 ? "prev3" : ""}`}
-                style={{
-                  transform: isActive
-                    ? "scale(1) translateX(0)"
-                    : isNext1
-                    ? "scale(0.9) translateX(100%)"
-                    : isNext2
-                    ? "scale(0.8) translateX(200%)"
-                    : isNext3
-                    ? "scale(0.7) translateX(300%)"
-                    : isPrev1
-                    ? "scale(0.9) translateX(-100%)"
-                    : isPrev2
-                    ? "scale(0.8) translateX(-200%)"
-                    : isPrev3
-                    ? "scale(0.7) translateX(-300%)"
-                    : "scale(0.5)",
-                  opacity: isActive ? 1 : (0 && isNext1) || isPrev1 ? 0.8 : 0.5,
-                  zIndex: isActive ? 10 : isNext1 || isPrev1 ? 5 : 0,
-                  margin: isNext2
-                    ? "0 200px"
-                    : isNext3
-                    ? "0 400px"
-                    : isPrev2
-                    ? "0 -200px"
-                    : isPrev3
-                    ? "0 -400px"
-                    : "0",
-                }}
-              >
-                {card}
-              </li>
-            );
-          })}
-        </ul>
+              return (
+                <li
+                  key={index}
+                  className={`card ${isActive ? "active" : ""} ${
+                    isNext1 ? "next1" : ""
+                  } ${isNext2 ? "next2" : ""} ${isNext3 ? "next3" : ""} ${
+                    isPrev1 ? "prev1" : ""
+                  } ${isPrev2 ? "prev2" : ""} ${isPrev3 ? "prev3" : ""}`}
+                  style={{
+                    transform: isActive
+                      ? "scale(1) translateX(0)"
+                      : isNext1
+                      ? "scale(0.8) translateX(100%)"
+                      : isNext2
+                      ? "scale(0.6) translateX(200%)"
+                      : isNext3
+                      ? "scale(0.5) translateX(300%)"
+                      : isPrev1
+                      ? "scale(0.8) translateX(-100%)"
+                      : isPrev2
+                      ? "scale(0.7) translateX(-200%)"
+                      : isPrev3
+                      ? "scale(0.5) translateX(-300%)"
+                      : "scale(0.5)",
+                    opacity: isActive
+                      ? 1
+                      : isNext1 || isPrev1
+                      ? 0.6
+                      : isNext2 || isPrev2 
+                      ? 0.4
+                      : isNext3 || isPrev3
+                      ? 0.1
+                      : 0,
 
-        <div className="actions flex gap-5">
-          <button
-            className="text-white bg-amber-300 w-16 h-16 rounded-xl"
-            onClick={handlePrev}
-          >
-            Prev
-          </button>
-          <button
-            className="text-white bg-amber-300 w-16 h-16 rounded-xl"
-            onClick={handleNext}
-          >
-            Next
-          </button>
+                    zIndex: isActive ? 10 : isNext1 || isPrev1 ? 5 : 0,
+                    margin: isNext2
+                      ? "0 200px"
+                      : isNext3
+                      ? "0 400px"
+                      : isPrev2
+                      ? "0 -200px"
+                      : isPrev3
+                      ? "0 -400px"
+                      : "0",
+                  }}
+                >
+                  {card}
+                </li>
+              );
+            })}
+          </ul>
+
+          <div className="actions flex gap-5">
+            <button
+              className="text-white bg-amber-300 w-16 h-16 rounded-xl"
+              onClick={handlePrev}
+            >
+              Prev
+            </button>
+            <button
+              className="text-white bg-amber-300 w-16 h-16 rounded-xl"
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="drag-proxy"></div>
+    </>
   );
 };
 
