1acbc56d2b182e759ef24c39181c2f898eccc3ba
riya-ws <riya@whitestorksoft.com>
2024-10-28 12:01:57 +0530

    Add lazy load in root file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/home";
-import ThirdTestimonials from "./pages/third-testimonials/third-testimonials";
-import Work from "./pages/work/work";
-import InfiniteCardScroll from "./pages/infinite-card-scroll/infinite-card-scroll";
-import InfiniteLogo from "./pages/infinite-logo/infinite-logo";
-import ScrollDriven from "./pages/scroll-driven/scroll-driven";
-import ScrollDrivenDemoTwo from "./pages/scroll-driven-demo-two/scroll-driven-demo-two";
-import ScrollDrivenDemoThree from "./pages/scroll-driven-demo-three/scroll-driven-demo-three";
-import ScrollReveal from "./pages/scroll-reveal/scroll-reveal";
+const Home = lazy(() => import("./pages/home/home"));
+const ThirdTestimonials = lazy(
+  () => import("./pages/third-testimonials/third-testimonials")
+);
+const InfiniteLogo = lazy(() => import("./pages/infinite-logo/infinite-logo"));
+
+const Work = lazy(() => import("./pages/work/work"));
+const InfiniteCardScroll = lazy(
+  () => import("./pages/infinite-card-scroll/infinite-card-scroll")
+);
+const ScrollDriven = lazy(() => import("./pages/scroll-driven/scroll-driven"));
+const ScrollDrivenDemoTwo = lazy(
+  () => import("./pages/scroll-driven-demo-two/scroll-driven-demo-two")
+);
+const ScrollDrivenDemoThree = lazy(
+  () => import("./pages/scroll-driven-demo-three/scroll-driven-demo-three")
+);
+const ScrollReveal = lazy(() => import("./pages/scroll-reveal/scroll-reveal"));
 
 function App() {
   return (
@@ -22,11 +32,20 @@ function App() {
           />
           <Route path="/on-scroll-sticky-card-stack" element={<Work />} />
           <Route path="/sticky-scroll-reveal" element={<ScrollReveal />} />
-          <Route path="/infinite-scrolling-card" element={<InfiniteCardScroll />} />
+          <Route
+            path="/infinite-scrolling-card"
+            element={<InfiniteCardScroll />}
+          />
           <Route path="/infinite-logo-slider" element={<InfiniteLogo />} />
           <Route path="/scroll-driven" element={<ScrollDriven />} />
-          <Route path="/scroll-driven-demo-two" element={<ScrollDrivenDemoTwo />} />
-          <Route path="/scroll-driven-demo-three" element={<ScrollDrivenDemoThree />} />
+          <Route
+            path="/scroll-driven-demo-two"
+            element={<ScrollDrivenDemoTwo />}
+          />
+          <Route
+            path="/scroll-driven-demo-three"
+            element={<ScrollDrivenDemoThree />}
+          />
         </Routes>
       </BrowserRouter>
     </>
