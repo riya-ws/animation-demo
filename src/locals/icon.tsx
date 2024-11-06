a75ad33c758a4ad29f8397a7de70dfb4837c4923
riya-ws <riya@whitestorksoft.com>
2024-11-06 10:15:11 +0530

    Changes in file

--- src/locals/icon.tsx
+++ src/locals/icon.tsx
@@ -35,8 +35,8 @@ export const Avatar = () => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      width="64"
-      height="64"
+      width="50"
+      height="50"
       fill="none"
       viewBox="0 0 48 48"
     >
