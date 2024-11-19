bcdb03713813e8bfb4c48f5ea47d81878611d9be
riya-ws <riya@whitestorksoft.com>
2024-11-19 14:15:29 +0530

    Card working properly but animation not working

--- src/pages/spotagency-doing/spotagency-doing.tsx
+++ src/pages/spotagency-doing/spotagency-doing.tsx
@@ -1,118 +1,20 @@
 import { useState } from "react";
 import { SpotLock, TopArrow } from "../../locals/icon";
 import { AgencyDoingData } from "../../utils/constant";
-
 const SpotagencyDoing = () => {
-  // const [activeCardIndex, setActiveCardIndex] = useState(null);
-
-  // const handleCardClick = (index: any) => {
-  //   setActiveCardIndex(index === activeCardIndex ? null : index); // Toggle active state
-  // };
   const [activeCardIndex, setActiveCardIndex] = useState(null);
+  // const [animateArrow, setAnimateArrow] = useState(false);
 
-  const handleCardClick = (index:any) => {
+  const handleCardClick = (index: any) => {
     setActiveCardIndex((prev) => (prev === index ? null : index)); // Toggle card open/close
   };
+  // useEffect(() => {
+  //   if (dataIndex !== AgencyDoingData.length - 1) {
+  //     setAnimateArrow(true);
+  //   }
+  // }, [dataIndex]);
   return (
     <>
-      {/* <div className="container mx-auto">
-        <h1 className="text-center text-6xl font-medium my-10">
-          What we are doing
-        </h1>
-        {AgencyDoingData.map((dataItem, dataIndex) => (
-          <div
-            key={dataIndex}
-            className="flex justify-between w-full p-5 rounded-3xl"
-            style={{
-              backgroundColor: dataItem.bgColor,
-              // transform:
-              //   dataIndex === activeCardIndex
-              //     ? "translate(0px, -360px)"
-              //     : dataIndex === activeCardIndex + 1
-              //     ? "translate(0px, -720px)"
-              //     : dataIndex === activeCardIndex + 2
-              //     ? "translate(0px, -1180px)"
-              //     : dataIndex === activeCardIndex + 3
-              //     ? "translate(0px, -1780px)"
-              //     : "",
-              // height: dataIndex === activeCardIndex ? "100%" : "auto",
-              // transition: "transform 0.5s ease, height 0.5s ease",
-            }}
-            onClick={() => handleCardClick(dataIndex)}
-          >
-            <div className="w-[30%]">
-              <h2 className="text-[40px] font-medium text-black max-w-60">
-                {dataItem.title}
-              </h2>
-            </div>
-            <div className="flex flex-col gap-5 w-[60%]">
-              <p className="text-xl font-normal max-w-4xl">
-                {dataItem.description}
-              </p>
-              {dataIndex !== AgencyDoingData.length - 1 && (
-                <div className="max-w-sm flex flex-wrap gap-3">
-                  {dataItem.button.map((btn, btnIndex) => (
-                    <span
-                      key={btnIndex}
-                      className="border-2 border-black rounded-3xl text-sm font-normal p-2"
-                    >
-                      {btn.text}
-                    </span>
-                  ))}
-                </div>
-              )}
-              {dataIndex !== AgencyDoingData.length - 1 && (
-                <div className="flex cursor-pointer">
-                  <div className="bg-white text-[17px] font-normal p-3 rounded-3xl flex justify-center items-center gap-3">
-                    See more
-                    <TopArrow
-                      width={12}
-                      height={12}
-                      // style={{
-                      //   transition: "transform 0.5s ease",
-                      //   transform:
-                      //     dataIndex === activeCardIndex
-                      //       ? "rotate(180deg)"
-                      //       : "rotate(0deg)",
-                      // }}
-                      // style={{
-                      //   transition: "transform 0.5s ease",
-                      //   transform:
-                      //     dataIndex === activeCardIndex
-                      //       ? "rotate(180deg)"
-                      //       : "rotate(0deg)",
-                      // }}
-                    />
-                  </div>
-                </div>
-              )}
-            </div>
-
-            <div className="w-[10%] flex justify-end">
-              {dataIndex === AgencyDoingData.length - 1 ? (
-                <SpotLock />
-              ) : (
-                <TopArrow
-                  width={46}
-                  height={46}
-                  // style={{
-                  //   transition: "transform 0.5s ease",
-                  //   transform:
-                  //     dataIndex === activeCardIndex
-                  //       ? "rotate(180deg)"
-                  //       : "rotate(0deg)",
-                  // }}
-                />
-              )}
-            </div>
-          </div>
-        ))}
-        <div className="text-center">
-          <button className="bg-black text-white text-base font-medium p-4 rounded-3xl">
-            In progress...
-          </button>
-        </div>
-      </div> */}
       <div className="container mx-auto">
         <h1 className="text-center text-6xl font-medium my-10">
           What we are doing
@@ -120,12 +22,17 @@ const SpotagencyDoing = () => {
         {AgencyDoingData.map((dataItem, dataIndex) => (
           <div
             key={dataIndex}
-            className={`flex flex-col md:flex-row justify-between w-full p-5 rounded-3xl transition-all duration-500 ease-in-out ${
+            className={`flex flex-col md:flex-row justify-between w-full p-16 rounded-3xl transition-transform duration-300 ease-in-out  ${
               activeCardIndex === dataIndex
                 ? "h-auto"
-                : "h-[120px] overflow-hidden"
+                : "h-[170px] overflow-hidden "
             }`}
-            style={{ backgroundColor: dataItem.bgColor }}
+            style={{
+              backgroundColor: dataItem.bgColor,
+              // zIndex: AgencyDoingData.length - dataIndex, // Higher index for cards at the top
+              marginTop: dataIndex === 0 ? 0 : "-40px", // Adjust overlap amount
+              position: "relative", // To allow stacking
+            }}
             onClick={() => handleCardClick(dataIndex)}
           >
             <div className="w-full md:w-[30%]">
@@ -161,21 +68,14 @@ const SpotagencyDoing = () => {
               {dataIndex === AgencyDoingData.length - 1 ? (
                 <SpotLock />
               ) : (
-                <TopArrow
-                  width={46}
-                  height={46}
-                  style={{
-                    transform:
-                      activeCardIndex === dataIndex
-                        ? "rotate(180deg)"
-                        : "rotate(0deg)",
-                    transition: "transform 0.5s ease",
-                  }}
-                />
+                // <div className={animateArrow ? "arrow-up-animation" : ""}>
+                <TopArrow width={46} height={46} />
+                // </div>
               )}
             </div>
           </div>
         ))}
+
         <div className="text-center">
           <button className="bg-black text-white text-base font-medium p-4 rounded-3xl">
             In progress...
