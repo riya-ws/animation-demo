6395a41950dba94fa214c1bd88a850dc9ba254fa
riya-ws <riya@whitestorksoft.com>
2024-12-04 11:53:25 +0530

    spot agency demo donebut small jurk in scrolling

--- src/pages/spotagency-infinite-scroll/spotagency-infinite-scroll.tsx
+++ src/pages/spotagency-infinite-scroll/spotagency-infinite-scroll.tsx
@@ -1,4 +1,5 @@
 import { SpotagencyScrollData } from "../../utils/constant";
+import "./spotagency-infinite-scroll.css";
 
 const SpotagencyInfiniteScroll = () => {
   return (
@@ -7,58 +8,97 @@ const SpotagencyInfiniteScroll = () => {
         <div className="container mx-auto ">
           <h1 className="text-7xl font-medium py-7">How do we do it</h1>
         </div>
-        <div className="space-y-5">
-          <div className="flex gap-[450px]">
-            <div className="flex gap-6">
-              {SpotagencyScrollData.map((item, index) => {
-                return (
-                  <div
-                    key={index}
-                    style={{backgroundColor : item.bgColor}}
-                    className="rounded-3xl p-5 w-[450px] h-60"
-                  >
-                    <p className="text-3xl font-medium max-w-80">
-                      {item.description}
+        <div
+          className="scroller spot-agency-infinite flex"
+          data-animated="true"
+          data-direction="left"
+          data-speed="slow"
+        >
+          <div className="scroller__inner_agency">
+            <div className="space-y-5">
+              <div className="flex gap-[450px]">
+                <div className="flex gap-6">
+                  {SpotagencyScrollData.map((item, index) => {
+                    return (
+                      <div
+                        key={index}
+                        style={{ backgroundColor: item.bgColor }}
+                        className={`rounded-3xl p-5 w-[450px] h-60 ${
+                          item.bgColor === "#0d0d0d"
+                            ? "text-white"
+                            : "text-black"
+                        }`}
+                      >
+                        <p className="text-3xl font-medium max-w-80">
+                          {item.description}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex gap-[300px]">
+                <div className="bg-[#bba9ff] rounded-3xl p-5 w-[450px] h-60">
+                  <p className="text-3xl font-medium max-w-80">
+                    With heart and love for your brand
+                  </p>
+                </div>
+                <div className="mt-28 flex gap-5">
+                  <div className="bg-[#ff611d] rounded-3xl p-5 w-[450px] h-60 ">
+                    <p className="text-3xl font-medium max-w-80 text-white">
+                      At the intersection of technology, research and wow
                     </p>
                   </div>
-                );
-              })}
+                  <div className="bg-[#ffffff] rounded-3xl p-5 w-[450px] h-60">
+                    <p className="text-3xl font-medium max-w-80">
+                      Transparent work process
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-            {/* <div className="flex gap-6">
-              <div className="bg-[#c9ff33] rounded-3xl p-5 w-[450px] h-60">
-                <p className="text-3xl font-medium max-w-80">
-                  Bringing value to the business by driving strategy and
-                  objectives
-                </p>
-              </div>
-              <div className="bg-[#0d0d0d] rounded-3xl p-5 w-[450px] h-60">
-                <p className="text-3xl font-medium max-w-80 text-white">
-                  Balancing aesthetics and logic
-                </p>
-              </div>
-              <div className="bg-[#bba9ff] rounded-3xl p-5 w-[450px] h-60">
-                <p className="text-3xl font-medium max-w-80">
-                  Payment in installments
-                </p>
-              </div>
-            </div> */}
           </div>
-          <div className="flex gap-[300px]">
-            <div className="bg-[#bba9ff] rounded-3xl p-5 w-[450px] h-60">
-              <p className="text-3xl font-medium max-w-80">
-                With heart and love for your brand
-              </p>
-            </div>
-            <div className="mt-28 flex gap-5">
-              <div className="bg-[#ff611d] rounded-3xl p-5 w-[450px] h-60 ">
-                <p className="text-3xl font-medium max-w-80 text-white">
-                  At the intersection of technology, research and wow
-                </p>
+          <div className="scroller__inner_agency">
+            <div className="space-y-5">
+              <div className="flex gap-[450px]">
+                <div className="flex gap-6">
+                  {SpotagencyScrollData.map((item, index) => {
+                    return (
+                      <div
+                        key={index}
+                        style={{ backgroundColor: item.bgColor }}
+                        className={`rounded-3xl p-5 w-[450px] h-60 ${
+                          item.bgColor === "#0d0d0d"
+                            ? "text-white"
+                            : "text-black"
+                        }`}
+                      >
+                        <p className="text-3xl font-medium max-w-80">
+                          {item.description}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-              <div className="bg-[#ffffff] rounded-3xl p-5 w-[450px] h-60">
-                <p className="text-3xl font-medium max-w-80">
-                  Transparent work process
-                </p>
+              <div className="flex gap-[300px]">
+                <div className="bg-[#bba9ff] rounded-3xl p-5 w-[450px] h-60">
+                  <p className="text-3xl font-medium max-w-80">
+                    With heart and love for your brand
+                  </p>
+                </div>
+                <div className="mt-28 flex gap-5">
+                  <div className="bg-[#ff611d] rounded-3xl p-5 w-[450px] h-60 ">
+                    <p className="text-3xl font-medium max-w-80 text-white">
+                      At the intersection of technology, research and wow
+                    </p>
+                  </div>
+                  <div className="bg-[#ffffff] rounded-3xl p-5 w-[450px] h-60">
+                    <p className="text-3xl font-medium max-w-80">
+                      Transparent work process
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
